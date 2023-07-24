1c6f2d20fb523961196595d82638abbe3e822303
Mufeez08 <56508719+Mufeez08@users.noreply.github.com>
2023-07-23 22:08:46 -0400

    added 2 api endpoints and related frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
-import './App.css';
+import {useState,useEffect} from "react";
+// import './App.css';
 
 function App() {
+  const [title,setTitle] = useState("");
+  const [decks,setDecks] = useState([]);
+
+  function handleCreateDeck(event){
+    event.preventDefault();
+    fetch("http://localhost:3001/deck",{
+      method:"POST",
+      body: JSON.stringify({
+        title: title,
+      }),
+      headers: {
+        "Content-Type":"application/json",
+      },
+    }).then(()=>{
+      setTitle("");
+    });
+
+  }
+  
+  useEffect(()=>{
+    async function fetchDecks() {
+      const response = await fetch("http://localhost:3001/deck");
+      console.log(response);
+      const newdecks = await response.json();
+      console.log(newdecks);
+      setDecks(newdecks);
+    }
+    fetchDecks();
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul className='decks'>
+        {decks.map((deck)=>(
+          <li key={deck._id}>{deck.title}</li>)
+          )}
+    </ul>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input id="deck-title" value={title} onChange={(event)=>{setTitle(event.target.value)}}/>
+        <button>Create Deck</button>
+      </form>
     </div>
   );
 }
--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import DeckModel from "./models/Deck.js";
+import cors from "cors";
 const app = express();
+
+app.use(cors({origin:"*"}));
 app.use(express.json());
 
 dotenv.config();
@@ -14,12 +17,18 @@ app.get("/", (req,res)=>{
     res.send("hello world");
 });
 
+app.get("/deck",async (req,res)=>{
+    const response = await DeckModel.find();
+    //console.log(response);
+    res.json(response);
+})
+
 app.post("/deck",async (req,res)=>{
     const newDeck = new DeckModel({
-        title: req.body.title
+        title: req.body.title 
     });
     const response = await newDeck.save();
-    console.log(response);
+    //console.log(response);
     res.json(response); 
 })
 
